69e7bd00181d117c17c5873f279656f11c3801d2
Dae Mon <125165282+XienDaeMon@users.noreply.github.com>
2023-02-26 06:22:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -32,20 +32,20 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "6281269734000";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
 
 
-global.owner = gg.split(",");
+global.owner = gg.split("6281269734000");
 global.mongodb = process.env.MONGODB || "mongodb+srv://fantox:xrto71r@cluster0.dnlowts.mongodb.net/?retryWrites=true&w=majority";
 global.sessionId = process.env.SESSION_ID || "ok";
-global.prefa = process.env.PREFIX || "-";
+global.prefa = process.env.PREFIX || ".";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas MD`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `DemonBot`;
+global.author = process.env.AUTHOR || "by: XienDaeMon";
 global.port = process.env.PORT || "8000";
 
 module.exports = {
